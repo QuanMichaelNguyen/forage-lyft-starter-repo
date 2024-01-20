e8c8b2f584bab9ba62865f3f0482e033db321c87
QuanMichaelNguyen <144957908+QuanMichaelNguyen@users.noreply.github.com>
2024-01-20 11:32:57 -0500

    Ba Quan Nguyen 3 testing

--- car.py
+++ car.py
@@ -1,88 +1,10 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
-class Engine:
+
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
     def needs_service(self):
-        pass
-class CapuletEngine(Engine):
-    def __init__(self) -> None:
-        super().__init__()
-        self.last_service_mileage = None
-        self.current_mileage = None
-    def needs_service(self):
-        return super().needs_service()
-    
-class WilloughbyEngine(Engine):
-    def __init__(self) -> None:
-        super().__init__()
-        self.last_service_mileage = None
-        self.current_mileage = None
-    def needs_service(self):
-        return super().needs_service()
-
-class StermanEngine(Engine):
-    def __init__(self) -> None:
-        super().__init__()
-        self.last_service_mileage = None
-        self.current_mileage = None
-    def needs_service(self):
-        return super().needs_service()
-
-class Battery:
-    def needs_service(self):
-        pass
-
-class SpindlerBattery(Battery):
-    def __init__(self) -> None:
-        super().__init__()
-        self.last_service_date = None
-        self.current_date = None
-    def needs_service(self):
-        return super().needs_service()
-
-class NubbinBattery(Battery):
-    def __init__(self) -> None:
-        super().__init__()
-        self.last_service_date = None
-        self.current_date = None
-    def needs_service(self):
-        return super().needs_service()
-    
-
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-        self.engine = Engine()
-        self.battery = Battery()
-    @abstractmethod
-    def needs_service(self):
-        pass
-
-class CarFactory(Car):
-    def create_calliope(self):
-        self.current_date = None
-        self.last_service_date = None
-        self.current_mileage = None
-        self.last_service_mileage = None
-
-    def create_glissade(self):
-        self.current_date = None
-        self.last_service_date = None
-        self.current_mileage = None
-        self.last_service_mileage = None
-    
-    def palindrome(self):
-        self.current_date = None
-        self.last_service_date = None
-        self.warning_light_on =  False
-
-    def create_rorschach(self):
-        self.current_date = None
-        self.last_service_date = None
-        self.current_mileage = None
-        self.last_service_mileage = None
-    
-    def create_thovex(self):
-        self.current_date = None
-        self.last_service_date = None
-        self.current_mileage = None
-        self.last_service_mileage = None
+        return self.engine.needs_service() or self.battery.needs_service()
